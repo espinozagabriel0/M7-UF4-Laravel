594c45f3e1d126711525c8d8d0aaa41194cbb68c
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2025-04-11 10:17:49 +0000

    api cards, falta comprobar endpoints

--- app/Http/Controllers/Api/CardsController.php
+++ app/Http/Controllers/Api/CardsController.php
@@ -9,19 +9,22 @@ use Illuminate\Support\Facades\Validator;
 
 class CardsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $cards = Cards::all();
         return response()->json(['cards' => $cards], 200);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $card = Cards::find($id);
         if (!$card) {
             return response()->json(['message' => "Carta no encontrada."], 400);
         }
         return response()->json(['carta' => $card], 200);
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         // Validar los datos de entrada
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
@@ -38,5 +41,40 @@ class CardsController extends Controller
 
         // Retornar la respuesta con el registro creado
         return response()->json(['carta' => $card], 201);
+    }
+    public function udpate(Request $request, $id)
+    {
+        $card = Cards::find($id);
+
+        // Verificar si la carta existe
+        if (!$card) {
+            return response()->json(['message' => 'Carta no encontrada.'], 404);
+        }
+
+        // Validar los datos de entrada
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'url' => 'required|string|unique:Cards,url',
+        ]);
+
+        // Comprobar si la validación falla
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        // Actualizar los datos de la carta
+        $card->update($request->all());
+
+        // Retornar la respuesta con la carta actualizada
+        return response()->json(['carta' => $card], 200);
+    }
+    public function destroy($id)
+    {
+        $carta = Cards::find($id);
+        if (!$carta) {
+            return response()->json(['message' => 'Carta no encontrada.'], 404);
+        }
+        $carta->delete();
+        return response()->json(['message' => 'Carta no encontrada.'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/cards', [CardsController::class, 'index']);
+Route::get('/cards/{id}', [CardsController::class, 'show']);
+Route::post('/cards', [CardsController::class, 'store']);
+Route::put('/cards/{id}', [CardsController::class, 'update']);
+Route::delete('/cards/{id}', [CardsController::class, 'destroy']);
