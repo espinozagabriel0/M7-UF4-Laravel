23631f3ada633a16e8ce73d2fde46d7ec2899dc7
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2025-05-28 14:49:51 +0000

    mvc book initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,19 +9,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/peliculas', function(){
-    return view('peliculas');
-});
+// Route::get('/peliculas', function(){
+//     return view('peliculas');
+// });
 
-Route::get('/suma', function(){
-    return view('suma');
-});
+// Route::get('/suma', function(){
+//     return view('suma');
+// });
 
-Route::post('/suma', function(Request $request){
-    $num1 = $request->input('num1');
-    $num2 = $request->input('num1');
+// Route::post('/suma', function(Request $request){
+//     $num1 = $request->input('num1');
+//     $num2 = $request->input('num1');
 
-    $resultado = $num1 + $num2;
+//     $resultado = $num1 + $num2;
 
-    return view('suma', ['resultado' => $resultado]);
-});
+//     return view('suma', ['resultado' => $resultado]);
+// });
+
+
+Route::resource('books', BookController::class);
