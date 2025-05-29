aa60e2788b6ceab5acba9c295ecf9aa3933ef02e
espinozagabriel0 <156692507+espinozagabriel0@users.noreply.github.com>
2025-05-29 16:24:40 +0000

    migración, modelo y controller (resource)  (mcr)

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class StudentController extends Controller
 {
